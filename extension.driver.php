febe88da113cc854f9022b04898f4712f856cd5b
michael-e <info@michael-eichelsdoerfer.de>
2011-02-11 17:45:46 +0100

    Releasing 1.1 (Symphony 2.2 compatibility, cleanup).

--- extension.driver.php
+++ extension.driver.php
@@ -6,37 +6,14 @@
 		{
 			return array(
 				'name' => 'HTTP (Response Header) Status Line Mappings',
-				'version' => '1.0',
-				'release-date' => '2010-03-10',
+				'version' => '1.1',
+				'release-date' => '2011-02-11',
 				'author'       => array(
 					'name'    => 'Michael Eichelsdoerfer',
 					'website' => 'http://www.michael-eichelsdoerfer.de',
 					'email'   => 'info@michael-eichelsdoerfer.de'
 				)
 			);
-		}
-
-		public function install()
-		{
-			$initial_mappings = array(
-				'503' => 'HTTP/1.1 503 Service Unavailable'
-			);
-			foreach($initial_mappings as $status => $header)
-			{
-				Administration::instance()->Configuration->set($status, $header, 'http-status-line-mappings');
-			}
-			$this->_Parent->saveConfig();
-		}
-
-		public function uninstall()
-		{
-			Administration::instance()->Configuration->remove('http-status-line-mappings');
-			Administration::instance()->saveConfig();
-		}
-
-		public function resolveStatus($status)
-		{
-			return $this->_Parent->Configuration->get($status, 'http-status-line-mappings');
 		}
 
 		public function getSubscribedDelegates()
@@ -48,6 +25,29 @@
 					'callback' => 'setContentType'
 				),
 			);
+		}
+
+		public function install()
+		{
+			$initial_mappings = array(
+				'503' => 'HTTP/1.1 503 Service Unavailable'
+			);
+			foreach($initial_mappings as $status => $header)
+			{
+				Symphony::Configuration()->set($status, $header, 'http-status-line-mappings');
+			}
+			Administration::instance()->saveConfig();
+		}
+
+		public function uninstall()
+		{
+			Symphony::Configuration()->remove('http-status-line-mappings');
+			Administration::instance()->saveConfig();
+		}
+
+		public function resolveStatus($status)
+		{
+			return Symphony::Configuration()->get($status, 'http-status-line-mappings');
 		}
 
 		public function setContentType(array $context=NULL)
